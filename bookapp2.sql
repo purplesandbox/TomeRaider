8b7a0e653c70af485e2e4de9a6b136a635429c07
16tales <128521409+16tales@users.noreply.github.com>
2023-05-16 20:44:16 +0100

    removing latest changes

--- bookapp2.sql
+++ bookapp2.sql
@@ -8,8 +8,6 @@ CREATE TABLE read_books(
     title VARCHAR(200),
     author VARCHAR(200),
     category AS genre VARCHAR(200),
-    lexile_min INTEGER,
-    lexile_max INTEGER,
     review MEDIUMTEXT,
     star_rating ENUM('1','2','3','4','5'),
     PRIMARY KEY (id)
@@ -20,8 +18,6 @@ CREATE TABLE to_read_books(
     title VARCHAR(200),
     author VARCHAR(200),
     category AS genre VARCHAR(200),
-    lexile_min INTEGER,
-    lexile_max INTEGER,
     PRIMARY KEY (id)
 );
 
