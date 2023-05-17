6c58e2d0c4c1380988684a7069779b87df1f5377
purplesandbox <122180183+purplesandbox@users.noreply.github.com>
2023-05-17 11:16:17 +0100

    Added comments to the methods and added self.request_params
    Added comments for every method, renamed the class, added missing user_input for the get_records(user_input), and added hardcoded self.request_params which gets bolted on the user_input dictionary to make the API calls.

--- API_results.py
+++ API_results.py
@@ -1,10 +1,14 @@
+from pprint import pprint
 import requests
 import random
-from pprint import pprint
 from urllib.error import HTTPError, URLError
 
-
-class BookFinderAPICalls:
+# """
+# Definition of the class which deals only with the external https://book-finder1.p.rapidapi.com/api API calls and returns results.
+# It takes values from the user input dictionary to initialise the API calls
+#
+# """
+class BookAppAPI:
     def __init__(self):
         self.endpoint = 'https://book-finder1.p.rapidapi.com/api/search'
         self.requirement = {
@@ -12,9 +16,15 @@ class BookFinderAPICalls:
     'X-RapidAPI-Host': 'book-finder1.p.rapidapi.com'
 }
         self.relevant_keys = ['authors', 'title', 'categories', 'summary']
+        self.request_params = {'results_per_page': '100', 'page': '1'}
 
+    # """
+    # make_api_request method calls the api, based on the user input parameters, handles potential errors and returns the records.
+    #
+    # """
     def make_api_request(self, user_input):
         try:
+            user_input.update(self.request_params)
             response = requests.get(self.endpoint, params=user_input, headers=self.requirement)
             records = response.json()
         except HTTPError as error:
@@ -29,7 +39,11 @@ class BookFinderAPICalls:
         else:
             return records
 
-
+    # """
+    # get_filtered_results method uses make_apy_request method to get the API request results,
+    # reduces to the requested number of records, specified by the user, and returns only the relevant information for those.
+    #
+    # """
     def get_filtered_results(self, user_input):
         try:
             books = int(user_input['book_num'])
@@ -44,6 +58,10 @@ class BookFinderAPICalls:
             random_sample_reduced = [{key: d[key] for key in self.relevant_keys} for d in random_sample]
             return random_sample_reduced
 
+    # """
+    # get_random_result method uses make_apy_request method to get the API request results,
+    # reduces it to a single, randomly chosen record, and returns only the relevant information for it.
+    # """
     def get_random_result(self, user_input):
         records = self.make_api_request(user_input)
         random_number = random.randrange(0, len(records['results']))
@@ -51,27 +69,19 @@ class BookFinderAPICalls:
         random_record_reduced = {key: random_record[key] for key in self.relevant_keys}
         return random_record_reduced
 
+    # """
+    # get_records method, based on the user's choice, captured in the user_input dictionary passed into the class,
+    # calls either get_filtered_results or
+    # get_random_result.
+    #
+    # @Maryam - maybe this method could be handled by the internal API??? It doesn't make sense to leave it here if it isn't ever used!
+    # """
 
     def get_records(self, user_input):
 
         if user_input['filtered_choice']:
-            return self.get_filtered_results()
+            return self.get_filtered_results(user_input)
 
         elif user_input['random_choice']:
-            return self.get_random_result()
+            return self.get_random_result(user_input)
 
-# user_input = {
-#     'author': None,
-#     'book_type': 'Fiction',
-#     'results_per_page': '100',
-#     'page': '1',
-#     'lexile-min': 1000,
-#     'lexile-max':2000,
-#     'categories': "Mystery & Suspense",
-#     'book_num': 5,
-#     'random_choice': False,
-#     'filtered_choice': True
-# }
-
-# book_finder = BookFinderAPICalls(user_input)
-# pprint(book_finder.get_records())
