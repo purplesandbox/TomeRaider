30e7ceb14b657181cdf927fa89f2953f519a1f53
MaryamA123 <maryam.asaria@gmail.com>
2023-05-19 17:41:43 +0100

    logic changed for checking if there are duplicates. The last one didn't work because we were reaching the limit of the API

--- internal_api.py
+++ internal_api.py
@@ -27,105 +27,32 @@ class InternalAPI:
     # Endpoint to get 1-10 book suggestions after user inputs search
     # @app.route('/books/search', methods=['GET'])
     def search_book_suggestions(self, user_input):
+        x = user_input['book_num']
+        user_input['book_num'] = 15
         user_input = self.clean_user_input(user_input)
-        # Call function to get a book suggestions
         book_suggestions = self.book_app_api.get_filtered_results(user_input)
 
-        # check for duplicates from to_read_list
-        book_suggestions = self.check_for_duplicates_from_to_read_list(user_input, book_suggestions)
+        # check for duplicates from to_read_list and return the number the person wanted
+        unique_book_suggestions = self.check_for_duplicates_from_to_read_list(x, book_suggestions)
 
-        # check for duplicates from read_list
-        book_suggestions = self.check_for_duplicates_from_read_list(user_input, book_suggestions)
+        # # check for duplicates from read_list
+        # book_suggestions = self.check_for_duplicates_from_read_list(user_input, book_suggestions)
 
         # Return book suggestion
-        return {'Suggestions': book_suggestions}
+        return {'Suggestions': unique_book_suggestions}
         # print this in user interactions class
 
-    # function to check where the books that have been searched for are already in the to_read_list. If they are,
-    # those books are replaced by a new search. If it goes through 3 searches and still can't find new books, it
-    # returns however many new books it can.
-    def check_for_duplicates_from_to_read_list(self, user_input, book_suggestions):
-        # Write code here to say if the suggestions are in the to_read_list, generate suggestions to replace
+    #function to give out a list of books with none of them being in the read_books section
+    def check_for_duplicates_from_to_read_list(self, x, book_suggestions):
         book_list = db_utils.get_all_books('to_read_books')
-        # a tuple of all the titles in the to_read_books list
-        titles_in_to_read_list = (book[0] for book in book_list)
-        # repeat_books is a tuple of book titles that have been suggested but are also in the to_read_books list
-        repeat_books_to_remove = (book_suggestion['title'] for book_suggestion in book_suggestions if
-                                  book_suggestion['title'] in titles_in_to_read_list)
-        # removing book suggestions that are already on the to_read list
-        book_suggestions = [book for book in book_suggestions if book['title'] not in repeat_books_to_remove]
+        # a list of all the titles in the to_read_books list
+        titles_in_to_read_list = [book[0] for book in book_list]
+        # repeat_books is a list of book titles that have been suggested but are also in the to_read_books list
+        book_suggestions = [book_suggestion['title'] for book_suggestion in book_suggestions if
+                                  book_suggestion['title'] not in titles_in_to_read_list]
 
-        # how many more books are needed to satisfy the users request given some have been removed as repeats?
-        number_to_replace = user_input['book_num'] - len(book_suggestions)
+        return book_suggestions[:x]
 
-        if number_to_replace == 0:
-            return book_suggestions  # No more replacements needed, return the suggestions
-
-        count = 0
-        while count < 3:
-            # call the external API to generate number_to_replace new responses
-            user_input['book_num'] = number_to_replace
-            new_book_suggestions = self.book_app_api.get_filtered_results(user_input)
-
-            if len(new_book_suggestions) == 0:
-                return {f"For your given criteria, it's only possible to return {len(book_suggestions)} books.": book_suggestions}
-
-            if len(book_suggestions) == 0:
-                book_suggestions = new_book_suggestions
-
-            # add the new_book_suggestions to the list of book_suggestions
-            book_suggestions += new_book_suggestions
-
-            count += 1
-
-        if count == 3:
-            return {f"For your given criteria, it's only possible to return {len(book_suggestions)} books.": book_suggestions}
-
-        return book_suggestions
-
-    # function to check whether the books that have been searched for are already in the read_list. If they are,
-    # those books are replaced by a new search. If it goes through 3 searches and still can't find new books, it
-    # returns however many new books it can.
-    def check_for_duplicates_from_read_list(self, user_input, book_suggestions):
-        # Write code here to say if the suggestions are in the to_read_list, generate suggestions to replace
-        book_list = db_utils.get_all_books('read_books')
-        # a tuple of all the titles in the to_read_books list
-        titles_in_to_read_list = (book[0] for book in book_list)
-        # repeat_books is a tuple of book titles that have been suggested but are also in the to_read_books list
-        repeat_books_to_remove = (book_suggestion['title'] for book_suggestion in book_suggestions if
-                                  book_suggestion['title'] in titles_in_to_read_list)
-        # removing book suggestions that are already on the to_read list
-        book_suggestions = [book for book in book_suggestions if book['title'] not in repeat_books_to_remove]
-
-        # how many more books are needed to satisfy the users request given some have been removed as repeats?
-        number_to_replace = user_input['book_num'] - len(book_suggestions)
-        # call the external API to generate number_to_replace new responses
-        if number_to_replace == 0:
-            return book_suggestions  # No more replacements needed, return the suggestions
-
-        count = 0
-        while count < 3:
-
-            user_input['book_num'] = number_to_replace
-            new_book_suggestions = self.book_app_api.get_filtered_results(user_input)
-
-            if len(new_book_suggestions) == 0:
-                return {
-                    f"For your given criteria, it's only possible to return {len(book_suggestions)} books.": book_suggestions}
-
-            if len(book_suggestions) == 0:
-                book_suggestions = new_book_suggestions
-
-            # add the new_book_suggestions to the list of book_suggestions
-            book_suggestions += new_book_suggestions
-
-            count += 1
-
-        if count == 3:
-            return {
-                f"For your given criteria, it's only possible to return {len(book_suggestions)} books.": book_suggestions}
-
-        return book_suggestions
 
     # Endpoint to get 1 random book suggestion
     # @app.route('/books/random_suggestion', methods=['GET'])
@@ -135,17 +62,13 @@ class InternalAPI:
 
         # Write code here to say if the suggestion is in the already_read_list, randomly generate another suggestion
 
-        # check for duplicates from to_read_list
+        # check for duplicates from to_read_list - need to change this to read list
         random_book = self.check_for_duplicates_from_to_read_list(user_input, random_book)
-
-        # check for duplicates from read_list
-        random_book = self.check_for_duplicates_from_read_list(user_input, random_book)
 
         # Return random_book
         return ({'Random Book': random_book})
 
-    #
-    #
+
     # Endpoint to add a book to the reading list
     # @app.route('/books/reading_list', methods=['POST'])
     def add_to_to_read_list(self, to_read):
@@ -215,12 +138,3 @@ class InternalAPI:
         #else user_rating is added
         return db_utils.update_rating(read['title'], user_rating)
 
-
-internal_api = InternalAPI()
-print(internal_api.check_for_duplicates_from_to_read_list(user_input={'book_num': 1, 'book_type': 'fiction'},
-                                                          book_suggestions=[
-                                                              {'title': 'The B.F.G', 'authors': 'Rhoal Dahl'}]))
-
-# internal_api = InternalAPI()
-# to_read_list = internal_api.get_to_read_list()
-# read_list = internal_api.get_read_list()
