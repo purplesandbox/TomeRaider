2a7c30a63bae48ee8da0014fd6e7579dc1d0f5d7
mwkan <112250198+mwkan@users.noreply.github.com>
2023-05-21 17:09:57 +0100

    add function to add book to to_read list

--- user_interactions.py
+++ user_interactions.py
@@ -5,8 +5,10 @@ from tabulate import tabulate
 
 """User interactions class containing internal API, and default values for book criteria"""
 
+
 class BookNotFound(Exception):
     pass
+
 
 class UserInteractions:
     def __init__(self):
@@ -102,22 +104,35 @@ class UserInteractions:
         self.book_criteria['categories'] = chosen_genre
         self.book_criteria['random_choice'] = True
 
-        pp(self.internal_api.random_book_suggestion(user_input=self.book_criteria))
+        random_book = self.internal_api.random_book_suggestion(user_input=self.book_criteria)
+        pp(random_book)
+        self.add_book_to_to_read_list(random_book)
+
+    def add_book_to_to_read_list(self, to_read):
+        while True:
+            add_or_not = input('Would you like you add to your to-read list? (y/n)')
+            if add_or_not == 'y':
+                self.internal_api.add_to_to_read_list(to_read)
+                break
+            elif add_or_not == 'n':
+                print("Nothing has been added to your to-read list.")
+                break
+            else:
+                print("Invalid input. Please enter 'y' or 'n'")
 
     def add_book_to_read_list(self):
         book_to_add_title = input("What's the title of the book you would like to add? ")
         book_to_add_author = input('Author: ')
         book_to_add_category = input('Category: ')
 
-        #add logic to say if error of BookAlreadyOnTable raised then message user to say the book is already on the list
+        # add logic to say if error of BookAlreadyOnTable raised then message user to say the book is already on the list
 
         self.read_book_dict['title'] = book_to_add_title
         self.read_book_dict['author'] = book_to_add_author
         self.read_book_dict['categories'] = book_to_add_category
 
         self.internal_api.add_to_read_list(self.read_book_dict)
-        self.user_review_and_call_star_rating(read = self.read_book_dict)
-
+        self.user_review_and_call_star_rating(read=self.read_book_dict)
 
         # while True:
         #     add_to_list_or_not = input('Would you like to add this book to a list? (y/n): ')
@@ -127,7 +142,6 @@ class UserInteractions:
         #         return False
         #     else:
         #         print('Invalid choice. Please try again.')
-
 
     def star_rating(self, read):
         while True:
@@ -148,8 +162,6 @@ class UserInteractions:
             if star_rating == 'n':
                 break
 
-
-
     def user_review_and_call_star_rating(self, read):
         while True:
             review = input('Would you like to add a review for this book? (y/n) ')
@@ -162,8 +174,6 @@ class UserInteractions:
                 self.star_rating(read)
                 break
 
-
-
     def view_to_read_list(self):
         to_read_list = self.internal_api.view_to_read_list()
         if not to_read_list:
@@ -174,7 +184,7 @@ class UserInteractions:
                 table.append([book[0], book[1], book[2]])
 
             headers = ['Title', 'Author', 'Category']
-            pp (tabulate(table, headers, tablefmt='grid'))
+            pp(tabulate(table, headers, tablefmt='grid'))
 
     def view_read_list(self):
         read_list = self.internal_api.get_read_list()
