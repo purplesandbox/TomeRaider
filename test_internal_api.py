835e9aaf7b592bb77a7eb55de6cb9e60284befba
MaryamA123 <maryam.asaria@gmail.com>
2023-05-24 00:53:54 +0100

    Created unit tests with mocking for some of the functions for the internal api

--- test_internal_api.py
+++ test_internal_api.py
@@ -1,6 +1,13 @@
 from unittest import TestCase, main, mock
+from unittest.mock import call
 
 from internal_api import InternalAPI
+
+import responses
+
+
+def mock_db_responses(read, to_read):
+    return lambda input: read if input == 'read_books' else to_read if input == 'to_read_books' else []
 
 
 class InternalAPITests(TestCase):
@@ -44,13 +51,62 @@ class InternalAPITests(TestCase):
         result = self.internal_api.clean_user_input(user_input)
         self.assertEqual(expected, result)
 
-    def test_search_book_suggestion_if_API_returns_book_on_to_read_list(self):
-        pass
+    @mock.patch("db_utils.get_all_books",
+                side_effect=mock_db_responses(
+                    read=[
+                        ('Before the coffee gets cold', 'Toshikazu Kawaguchi', 'fiction',
+                         'I loved the atmosphere in the book, so dreamy, but also full of emotions', '4'),
+                        ('Talking to strangers', 'Malcolm Gladwell', 'nonfiction', None, '5'),
+                        ('The B.F.G', 'Roald Dahl', 'Animals, Bugs & Pets', 'Really enjoyed this book! ', '4'),
+                        ('Wilderness tips', 'Margaret Atwood', 'fiction', 'A short stories anthology', '3')
+                    ],
+                    to_read=[
+                        ('The Witches', 'Roald Dahl', 'Science Fiction & Fantasy')
+                    ]
+                ))
+    @responses.activate
+    def test_search_book_suggestion_no_duplicates(self, mock_get_all_books):
+        self.internal_api = InternalAPI()
+        book_results = [
+            {
+                'authors': ['Roald Dahl'],
+                'title': 'The Magic Finger',
+                'categories': ['Hobbies, Sports & Outdoors',
+                               'Fiction, Non-fiction & Poetry',
+                               'Science Fiction & Fantasy'],
+                'summary': 'The Gregg family loves hunting, but their eight-year-old '
+                           "neighbor can't stand it. After countless pleas for them to stop "
+                           'are ignored, she has no other choice -- she has to put her magic '
+                           'finger on them. Now the Greggs are a family of birds, and like '
+                           "it or not, they're going to find out how it feels to be on the "
+                           'other end of the gun.'
+            }
+        ]
+        search_results = {
+            'total_results': 1,
+            'total_pages': 1,
+            'results': book_results
+        }
+        responses.get(
+            url='https://book-finder1.p.rapidapi.com/api/search',
+            json=search_results,
+            status=200
+        )
+        user_input = {
+            'author': 'Roald Dahl',
+            'categories': 'Fiction, Non-fiction & Poetry',
+            'book_type': 'Fiction',
+            'book_num': 1}
+        self.assertEqual(self.internal_api.search_book_suggestions(user_input=user_input), book_results)
+        # mock_get_all_books.assert_has_calls([call('read_books'), call('to_read_books')])
 
     def test_search_book_suggestion_if_API_returns_book_on_read_list(self):
         pass
 
     def test_search_book_suggestion_returns_right_number_of_books(self):
+        pass
+
+    def test_search_book_suggestion_returns_books_even_though_results_give_less_than_asked_for(self):
         pass
 
     def test_search_book_suggestion_if_no_books_returned(self):
@@ -104,9 +160,29 @@ class InternalAPITests(TestCase):
     def delete_from_to_read_list_when_book_is_on_list(self):
         pass
 
-    def test_get_read_list_returns_list(self):
-        pass
+    @mock.patch('db_utils.get_all_books')
+    def test_get_read_list_returns_list(self, mock_get_all_books):
+        read_books = [('The Witches', 'Roald Dahl', 'Science Fiction & Fantasy')]
+        mock_get_all_books.return_value = read_books
+        self.internal_api = InternalAPI()
+        result = self.internal_api.get_read_list()
+        self.assertEqual(result, read_books)
+        mock_get_all_books.assert_called_once_with(table='read_books')
 
+    @mock.patch('db_utils.get_all_books')
+    def test_get_to_read_list_returns_list(self, mock_get_all_books):
+        to_read_books = [('The Witches', 'Roald Dahl', 'Science Fiction & Fantasy')]
+        mock_get_all_books.return_value = to_read_books
+        self.internal_api = InternalAPI()
+        result = self.internal_api.get_to_read_list()
+        self.assertEqual(result, to_read_books)
+        mock_get_all_books.assert_called_once_with(table='to_read_books')
+
+    # @mock.patch('shop.how_much_extra_money')
+    # def test_validate_extra_with_invalid_input(self, mock_how_much_extra_money):
+    #     mock_how_much_extra_money.return_value = 'blabla'
+    #     with self.assertRaises(ValueError):
+    #         get_extra_money()
     def test_add_a_review_for_book_not_on_list(self):
         pass
 
@@ -118,9 +194,6 @@ class InternalAPITests(TestCase):
 
     def test_add_star_rating_for_book_on_list(self):
         pass
-
-
-
 
     # def test_add_to_to_read_list(self):
     #     self.internal_api = InternalAPI()
@@ -140,7 +213,6 @@ class InternalAPITests(TestCase):
     #     self.internal_api = InternalAPI()
     #     self.book_app_api = BookAppAPI()
 
-
     # def test_add_to_to_read_list_1(self):
     #     self.internal_api = InternalAPI()
     #     to_read = {
@@ -151,10 +223,6 @@ class InternalAPITests(TestCase):
     #     expected = 'The B.F.G has been added to reading list'
     #     result = self.internal_api.add_to_to_read_list(to_read)
     #     self.assertEqual(expected, result)
-
-
-
-
 
 
 if __name__ == "__main__":
