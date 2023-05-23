7be1d6d0bb76658ecb9912129fbdd04e75fb0f25
MaryamA123 <maryam.asaria@gmail.com>
2023-05-23 13:55:51 +0100

    Created unit test titles for all functions on the internal api - now need to write the actual tests

--- test_internal_api.py
+++ test_internal_api.py
@@ -44,6 +44,83 @@ class InternalAPITests(TestCase):
         result = self.internal_api.clean_user_input(user_input)
         self.assertEqual(expected, result)
 
+    def test_search_book_suggestion_if_API_returns_book_on_to_read_list(self):
+        pass
+
+    def test_search_book_suggestion_if_API_returns_book_on_read_list(self):
+        pass
+
+    def test_search_book_suggestion_returns_right_number_of_books(self):
+        pass
+
+    def test_search_book_suggestion_if_no_books_returned(self):
+        pass
+
+    def test_check_for_duplicates_from_read_list_when_no_duplicates(self):
+        pass
+
+    def test_check_for_duplicates_from_read_list_when_one_duplicate(self):
+        pass
+
+    def test_check_for_duplicates_from_read_list_when_multiple_duplicates(self):
+        pass
+
+    def test_check_for_duplicates_from_to_read_list_when_no_duplicates(self):
+        pass
+
+    def test_check_for_duplicates_from_to_read_list_when_one_duplicate(self):
+        pass
+
+    def test_check_for_duplicates_from_to_read_list_when_multiple_duplicates(self):
+        pass
+
+    def test_check_random_suggestion_in_to_read_list_to_return_false(self):
+        pass
+
+    def test_check_random_suggestion_in_to_read_list_to_return_true(self):
+        pass
+
+    def test_check_random_suggestion_in_read_list_to_return_false(self):
+        pass
+
+    def test_check_random_suggestion_in_read_list_to_return_true(self):
+        pass
+
+    def test_add_to_to_read_list_when_adding_a_book(self):
+        pass
+
+    def test_add_to_to_read_list_when_book_already_on_table(self):
+        pass
+
+    def test_add_to_read_list_when_adding_a_book(self):
+        pass
+
+    def test_add_to_read_list_when_book_already_on_table(self):
+        pass
+
+    def delete_from_to_read_list_when_book_not_on_list(self):
+        pass
+
+    def delete_from_to_read_list_when_book_is_on_list(self):
+        pass
+
+    def test_get_read_list_returns_list(self):
+        pass
+
+    def test_add_a_review_for_book_not_on_list(self):
+        pass
+
+    def test_add_a_review_for_book_on_list(self):
+        pass
+
+    def test_add_star_rating_for_book_not_on_list(self):
+        pass
+
+    def test_add_star_rating_for_book_on_list(self):
+        pass
+
+
+
 
     # def test_add_to_to_read_list(self):
     #     self.internal_api = InternalAPI()
